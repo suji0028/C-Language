34dd14f882d07eff5a394d450236ffd637c5d5d7
suji0028 <154439826+suji0028@users.noreply.github.com>
2024-10-30 12:37:13 +0530

    Area of Rectangle

---
+++ Area_of_Rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int l,b;
+
+    printf("Enter Length of Rectangle:\n");
+    scanf("%d",&l);
+
+    printf("Enter Breadth of Rectangle:\n");
+    scanf("%d",&b);
+
+    printf("Area of Rectangle is %d",l*b);
+    return 0;
+}
