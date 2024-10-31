7016b7c03dcf183c9d3587577549dcb9026ed586
suji0028 <154439826+suji0028@users.noreply.github.com>
2024-10-31 17:16:34 +0530

    Function Quiz 3

---
+++ Function_quiz2.c
@@ -0,0 +1,17 @@
+// write a function to calculate force of attraction on a body of mass ‘m’ exerted by 
+// earth. Consider g = 9.8m/s2
+
+
+#include <stdio.h>
+
+float force(float);
+
+float force(float mass){
+    return mass*9.8;
+}
+
+int main(){
+    int m = 45;
+    printf("The value of force is %f\n", force(m));
+    return 0;
+}
