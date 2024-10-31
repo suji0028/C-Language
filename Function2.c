08ed9ab3e392714e231599899ebe5ff890264708
suji0028 <154439826+suji0028@users.noreply.github.com>
2024-10-31 17:08:38 +0530

    Function Quick Quiz

---
+++ Function2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int a;
+    printf("Enter Number:");
+    scanf("%d",&a);
+    printf("The area of this square is %f\n", pow(a, 2));
+    return 0;
+}
